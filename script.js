c7800cf05a3c51bb7ebcfa05d55bdd5ca2b6a20a
leprof <prof.violet.club@gmail.com>
2025-08-20 19:36:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ fetch('countries.geojson')
       onEachFeature: onCountryClick
     }).addTo(map);
   });
+
+function onCountryClick(feature, layer) {
+  layer.on('click', () => {
+    const countryName = feature.properties.ADMIN || 'Pays inconnu';
+    
+    // Appel à une API pour récupérer les infos détaillées
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+      .then(res => res.json())
+      .then(info => {
+        const country = info[0];
+        const popupContent = `
+          🇺🇳 <strong>${country.name.official}</strong><br>
+          🏳️ <img src="${country.flags.svg}" width="50"><br>
+          🧑‍⚖️ Chef d’État : ${country.headOfState || 'N/A'}<br>
+          👥 Population : ${country.population.toLocaleString()}<br>
+          🌍 Superficie : ${country.area.toLocaleString()} km²<br>
+          🏛️ Organisations : ONU, UNESCO, OMS (à compléter)<br>
+          📍 Capitale : ${country.capital}<br>
+          🗣️ Langue(s) : ${Object.values(country.languages).join(', ')}<br>
+          💰 Monnaie : ${Object.values(country.currencies)[0].name}<br>
+          📈 PIB : (à intégrer via autre API)<br>
+          📊 IDH : (à intégrer via autre source)
+        `;
+        layer.bindPopup(popupContent).openPopup();
+      });
+  });
+}
