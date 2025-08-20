13556914c7d7bd59d45822a4772d761511db928e
leprof <prof.violet.club@gmail.com>
2025-08-20 19:33:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,3 @@ const map = L.map('map').setView([43.2965, 5.3698], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
-
-// Ajout d’un marqueur avec popup
-L.marker([43.2965, 5.3698]).addTo(map)
-  .bindPopup('Bienvenue à Marseille !')
-  .openPopup();
